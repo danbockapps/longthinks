cd62ad1497637f6b01751b14efde35c9fabf4058
Dan Bock <danbock@gmail.com>
2023-08-27 08:03:35 -0400

    Rip out nextjs generated stuff

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import Boards from './Boards'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main>
       <Boards />
     </main>
   )
