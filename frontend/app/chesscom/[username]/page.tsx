643ba0a151d8e91e3ae93a4503b863337717cc2d
Dan Bock <danbock@gmail.com>
2023-08-29 21:04:49 -0400

    Add routes

---
+++ frontend/app/chesscom/[username]/page.tsx
@@ -0,0 +1,10 @@
+import Template from '@/src/Template'
+import { FC } from 'react'
+
+interface Props {
+  params: { username: string }
+}
+
+const Page: FC<Props> = props => <Template site='chesscom' username={props.params.username} />
+
+export default Page
