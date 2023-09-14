2353c26e592b5d7351f0a416563d13daf7adb66f
Nate Solon <nate@Nates-MacBook-Pro.local>
2023-09-14 13:50:09 -0500

    chess.com support

--- backend/get_games/chesscom.py
+++ backend/get_games/chesscom.py
@@ -1,1 +1,11 @@
-#TODO
+import requests
+from datetime import datetime as dt
+
+
+def get_games(username):
+    now = dt.now()
+    y = str(now.year)
+    m = str(now.month).zfill(2)
+    url = f"https://api.chess.com/pub/player/{username}/games/{y}/{m}/pgn"
+    r = requests.get(url, headers={"user-agent": "nate.solon@gmail.com"})
+    return r.text
