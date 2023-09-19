f269ddebb4dafde36b0e1a08e365142456ef09b2
Dan Bock <danbock@gmail.com>
2023-09-19 18:41:39 -0400

    Add .htaccess content in comment

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -25,3 +25,11 @@ const Page: FC = () => {
 }
 
 export default Page
+
+// .htaccess file in ~/public_html/longthinks:
+// # Don't rewrite files or directories
+// RewriteCond %{REQUEST_FILENAME} -f [OR]
+// RewriteCond %{REQUEST_FILENAME} -d
+// RewriteRule ^ - [L]
+// # Rewrite everything else to index.html to allow html5 state links
+// RewriteRule ^ index.html [L]
